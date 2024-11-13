c0c4e80faff43003fd195a7b3d75bf792680ca7e
Sfacius-DAWI <sfaciusangulo@cifpfbmoll.eu>
2024-11-13 17:04:37 +0100

    se ha metido una función para calcular una notificacion

--- conciertos.js
+++ conciertos.js
@@ -2,21 +2,21 @@ let conciertos = [
   {
     artista: "melendi",
     lugar: "palacio vistaalegre",
-    fecha: "12-11-2024",
+    fecha: new Date("2024-11-12T00:00:00"),
     limite_entradas: 1000,
     cancelable: true,
   },
   {
     artista: "ac/dc",
     lugar: "palacio vistaalegre",
-    fecha: "07-17-2026",
+    fecha: new Date("2026-07-17T00:00:00"),
     limite_entradas: 20000,
     cancelable: true,
   },
   {
     artista: "estopa",
     lugar: "palacio vistaalegre",
-    fecha: "12-11-2025",
+    fecha: new Date("2025-12-11T00:00:00"),
     limite_entradas: 700,
     cancelable: true,
   },
@@ -230,9 +230,49 @@ function crearDescripcion() {
     "<br>";
 }
 
-function Enviar() {
+async function programarNotificación(tiempo, artista) {
+  const hora_minutos = tiempo.split(":");
+  const hora = hora_minutos[0];
+  const minutos = hora_minutos[1];
+
+  const tiempoActual = new Date();
+  const notificaciónHora = new Date();
+
+  notificaciónHora.setHours(hora);
+  notificaciónHora.setMinutes(minutos);
+  notificaciónHora.setSeconds(0);
+  notificaciónHora.setMilliseconds(0);
+
+  const tiempoRestante = notificaciónHora - tiempoActual;
+
+  while (tiempoRestante > 0) {
+    if (tiempoRestante <= 300000) { // Menos de 5 minutos
+      await new Promise((resolve) => setTimeout(resolve, tiempoRestante));
+      alert(`Tu concierto de ${artista} está por comenzar.`);
+      break;
+    } else {
+      await new Promise((resolve) => setTimeout(resolve, 300000)); // Espera 5 minutos
+      tiempoRestante = notificaciónHora - new Date();
+    }
+  }
+  }
+
+
+function checkbox () {
+  let checkbox = document.getElementById("recordatorio_dia");
+  if (checkbox.checked) {
+  
+  } else {
+    document.getElementById("entradas_vip").value = 0;
+    document.getElementById("entradas_general").value = 0;
+  }
+}
+
+
+async function Enviar() {
   document.getElementById("respuesta").innerHTML = "";
   calculoCompra();
   calcularDiasRestantes();
   crearDescripcion();
+  await tiempo_limite();
 }
